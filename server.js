b692a2284361f8b2ba569a25f3fee08d35436c01
Vernon Williams <voxenn@gmail.com>
2022-01-27 18:04:44 -0600

    Add db and implement beginning of method

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const url = require('url').URL;
+let mongoose = require('mongoose');
+const { Schema } = mongoose;
+const urlSchema = new Schema ({
+    originalUrl: {type: String},
+    shortUrl: Number
+});
+mongoose.connect(process.env.MONGU_URI, { useNewUrlParser: true});
+let URL = mongoose.model('URL', urlSchema);
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -19,6 +27,30 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+//url validation method
+const isURLValid = (s) => {
+    try {
+        new URL(s);
+        return true;
+    } catch (err) {
+        return false;
+    }
+};
+
+//This method is designed to provide a response for a short url
+app.post('/api/shorturl', (req, res) => {
+    if(!isURLValid(req.original_url)) { res.json({"error": "invalid url"})};
+    const oldURL = new URL({ originalUrl: req.original_url, shortUrl: Math.floor(Math.random() * max) });
+    oldURL.save((err, date) => {
+        if(err) return console.err();
+        done(null, data);
+    });
+    next();
+}, (req, res) => {
+    res.json({
+        console.log('test');
+    });
+});
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
